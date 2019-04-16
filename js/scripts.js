864c396c399d6c163034e364d522890087f9fddc
Marc Davies and Marina Poltorak <marc@>
2019-04-16 16:37:00 -0700

    move newscript to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //Business Logic --------------------------------------------------------------
 
-var secretNumber = Math.floor(Math.random() * 1000);
-// console.log("secretNumber", secretNumber);
+var secretNumber = 100;
+// var secretNumber = Math.floor(Math.random() * 1000);
+console.log("secretNumber", secretNumber);
 
 function compareNumbers(guessedNumber, secretNumber, attempt) {
   if(guessedNumber === secretNumber) {
@@ -14,58 +15,88 @@ function compareNumbers(guessedNumber, secretNumber, attempt) {
     $("#underOverOutput").text("It's higher!");
     // console.log("Guess again, it's higher");
   }
-  var difference = Math.abs(guessedNumber - secretNumber);
-  // console.log("difference", difference);
+  var difference = Math.abs((guessedNumber/secretNumber));
+  console.log("difference", difference);
   if((guessedNumber === secretNumber)) {
+    $("body").removeClass();
     $("#output").removeClass();
-    $("#output").addClass("success");
+    $("body").addClass("success");
+    $("#output").addClass("white");
     $("#hotColdOutput").text("");
     if(countGuess !== 1){
     $("#attemptOutput").text("It took you " + attempt.attempts[attempt.attempts.length - 1].id + " attempts");
   } else $("#attemptOutput").text("It took you 1 attempt");
     // console.log("No text needed");
-  } else if(difference < 5) {
+  } else if(difference >= 0.95 && difference <= 1.05) {
+    $("body").removeClass();
     $("#output").removeClass();
-    $("#output").addClass("within5");
-    $("#hotColdOutput").text("You are within 5");
+    $("body").addClass("within5");
+    $("#hotColdOutput").text("You are within 5%");
     // console.log("You are within 10");
-  } else if(difference < 10) {
+  } else if(difference >= 0.9 && difference <= 1.1) {
+    $("body").removeClass();
     $("#output").removeClass();
-    $("#output").addClass("within10");
-    $("#hotColdOutput").text("You are within 10");
+    $("body").addClass("within10");
+    $("#hotColdOutput").text("You are within 10%");
     // console.log("You are within 10");
-  } else if (difference < 25) {
+  } else if (difference >= 0.8 && difference <= 1.2) {
+    $("body").removeClass();
     $("#output").removeClass();
-    $("#output").addClass("within25");
-    $("#hotColdOutput").text("You are within 25");
+    $("body").addClass("within20");
+    $("#hotColdOutput").text("You are within 20%");
     // console.log("You are within 50");
-  } else if (difference < 50) {
+  } else if (difference >= 0.7 && difference <= 1.3) {
+    $("body").removeClass();
     $("#output").removeClass();
-    $("#output").addClass("within50");
-    $("#hotColdOutput").text("You are within 50");
+    $("body").addClass("within30");
+    $("#hotColdOutput").text("You are within 30%");
     // console.log("You are within 50");within10
-  } else if (difference < 100) {
+  } else if (difference >= 0.6 && difference <= 1.4) {
+    $("body").removeClass();
     $("#output").removeClass();
-    $("#output").addClass("within100");
-    $("#hotColdOutput").text("You are within 100");
+    $("body").addClass("within40");
+    $("#hotColdOutput").text("You are within 40%");
     // console.log("You are within 100");
-  } else if (difference < 250) {
+  } else if (difference >= 0.5 && difference <= 1.5) {
+    $("body").removeClass();
     $("#output").removeClass();
-    $("#output").addClass("within250");
-    $("#hotColdOutput").text("You are within 250");
+    $("body").addClass("within50");
+    $("#hotColdOutput").text("You are within 50%");
     // console.log("You are within 250");
-  } else if (difference < 500) {
+  } else if (difference >= 0.4 && difference <= 1.6) {
+    $("body").removeClass();
     $("#output").removeClass();
-    $("#output").addClass("within500");
-    $("#hotColdOutput").text("You are within 500");
+    $("body").addClass("within60");
+    $("#hotColdOutput").text("You are within 60%");
     // console.log("You're within 500");
-  } else if (difference < 1000) {
+  } else if (difference >= 0.3 && difference <= 1.7) {
+    $("body").removeClass();
     $("#output").removeClass();
-    $("#output").addClass("notwithin500");
-    $("#hotColdOutput").text("You're not even within 500!");
+    $("body").addClass("within70");
+    $("#hotColdOutput").text("You are within 70%");
     // console.log("You're not even within 500!");
+  } else if (difference >= 0.2 && difference <= 1.8) {
+    $("body").removeClass();
+    $("#output").removeClass();
+    $("body").addClass("within80");
+    $("#hotColdOutput").text("You are within 80%");
+    // console.log("You're not even within 500!");
+  } else if (difference >= 0.1 && difference <= 1.9) {
+    $("body").removeClass();
+    $("#output").removeClass();
+    $("body").addClass("within90");
+    $("#hotColdOutput").text("You are within 90%!");
+    console.log("You're not even within 500!");
+  } else {
+    $("body").removeClass();
+    $("#output").removeClass();
+    $("body").addClass("tooFar");
+    $("#hotColdOutput").text("You are too far away!");
   }
 }
+
+
+
 var countGuess = 0;
 function Attempts() {
   this.attempts = [],
