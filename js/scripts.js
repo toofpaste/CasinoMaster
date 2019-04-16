7b1a33026f194814bb6fdc807edb01b42dff98c2
Marc Davies and Marina Poltorak <marc@>
2019-04-16 10:19:44 -0700

    add functionality to hotcold output and refactor classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,57 @@
 //Business Logic --------------------------------------------------------------
 
-var secretNumber = Math.floor(Math.random() * 100);
+var secretNumber = Math.floor(Math.random() * 1000);
 console.log("secretNumber", secretNumber);
 
 function compareNumbers(guessedNumber, secretNumber) {
   if(guessedNumber === secretNumber) {
-    $("#textOutput").text("Congratulations, you've WON!")
-    console.log("compareNumbers function", "Congratulations, you've WON!")
+    $("#underOverOutput").text("Congratulations, you guessed the number!")
+    // console.log("compareNumbers function", "Congratulations, you've WON!")
   } else if (guessedNumber > secretNumber){
-    $("#textOutput").text("Guess again, it's lower!");
-    console.log("Guess again, it's lower!")
+    $("#underOverOutput").text("It's lower!");
+    // console.log("Guess again, it's lower!")
   } else if(guessedNumber < secretNumber) {
-    $("#textOutput").text("Guess again, it's higher!");
-    console.log("Guess again, it's higher");
+    $("#underOverOutput").text("It's higher!");
+    // console.log("Guess again, it's higher");
   }
   var difference = Math.abs(guessedNumber - secretNumber);
   console.log("difference", difference);
-  if(difference < 10) {
-    $("span").addClass("burning");
-    console.log("You are burning, so close");
-  } else if (difference < 50 && difference > 10) {
-    $("span").addClass("hot");
-    console.log("You're hot");
-  } else if (difference < 100 && difference > 50) {
-    $("span").addClass("warm");
-    console.log("Pretty warm");
-  } else if (difference < 250 && difference > 100) {
-    $("span").addClass("chilly");
-    console.log("You're getting chilly");
-  } else if (difference < 500 && difference > 250) {
-    $("span").addClass("cold");
-    console.log("You're so cold!");
-  } else if (difference < 1000 && difference > 500) {
-    $("span").addClass("freezing");
-    console.log("You're freezing!");
+  if((guessedNumber === secretNumber)) {
+    $("#output").addClass("success");
+    $("#hotColdOutput").text("");
+    // console.log("No text needed");
+  } else if(difference < 5) {
+    $("#output").addClass("within5");
+    $("#hotColdOutput").text("You are within 5");
+    // console.log("You are within 10");
+  } else if(difference < 10) {
+    $("#output").addClass("within10");
+    $("#hotColdOutput").text("You are within 10");
+    // console.log("You are within 10");
+  } else if (difference < 25) {
+    $("#output").addClass("within25");
+    $("#hotColdOutput").text("You are within 25");
+    // console.log("You are within 50");
+  } else if (difference < 50) {
+    $("#output").addClass("within50");
+    $("#hotColdOutput").text("You are within 50");
+    // console.log("You are within 50");within10
+  } else if (difference < 100) {
+    $("#output").addClass("within100");
+    $("#hotColdOutput").text("You are within 100");
+    // console.log("You are within 100");
+  } else if (difference < 250) {
+    $("#output").addClass("within250");
+    $("#hotColdOutput").text("You are within 250");
+    // console.log("You are within 250");
+  } else if (difference < 500) {
+    $("#output").addClass("within500");
+    $("#hotColdOutput").text("You are within 500");
+    // console.log("You're within 500");
+  } else if (difference < 1000) {
+    $("#output").addClass("notwithin500");
+    $("#hotColdOutput").text("You're not even within 500!");
+    // console.log("You're not even within 500!");
   }
 }
 
